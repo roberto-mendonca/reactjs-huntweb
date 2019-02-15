7a02f22f36f699dbd30c7f0ea0a30121559c559e
Roberto Mendonça <robertomendonca20@gmail.com>
2019-02-15 14:25:27 -0200

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {  } from 'react';
+import Header from './components/Header'
+import Main from './pages/main'
+import Routes from './routes'
+import './style.css'
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+)
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.css'
+
+
+const Header = () => (
+    <header id="main-header">JS Hunt</header>
+)
+
+
+export default Header
